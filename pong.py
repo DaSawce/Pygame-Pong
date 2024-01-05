4081dd7a6af01c4891e47475826c5fba7652532c
David Romanillos <davidromanillos18@gmail.com>
2024-01-05 10:23:38 +0000

    1

--- pong.py
+++ pong.py
@@ -7,11 +7,34 @@ WIDTH = 700
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG")
 
+FPS = 60
+
+WHITE = (255,255,255)
+BLACK = (255,255,255)
+
+class Paddle:
+    COLOR = WHITE
+    def __init__(self, x ,y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+        def draw(self, WINDOW):
+            pygame.draw.rectangle(win, self.COLOR, ())
+
+def draw(win):
+    win.fill(BLACK)
+    pygame.display.update()
 
 def main ():
     run = True
+    clock = pygame.time.Clock()
     
     while run:
+        clock.tick(FPS)
+        draw(WINDOW)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -19,3 +42,6 @@ def main ():
             
     pygame.quit()
         
+        
+if __name__ == '__main__':
+    main()
